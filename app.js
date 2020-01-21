e34df7d767d943b9d52407bd6153e31da4c036f0
junghyun park <junghyun.de@gmail.com>
2020-01-22 02:15:51 +0900

    Setup Routes

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+import routes from './routes';
 
 const app = express();
-const PORT = 4000;
-
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-const handleHome = (req, res) => {
-  res.send('Hello! from home');
-}
-
-const handleProfile = (req, res) => {
-  res.send('profile');
-}
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -25,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get('/profile', handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;

f5d41bbec1fb537543d6b31e73bb454b8765ee93
junghyun park <junghyun.de@gmail.com>
2020-01-29 02:45:55 +0900

    add a dummy data

--- README.md
+++ README.md
@@ -4,14 +4,13 @@ Cloning Youtube with Vanilla and NodeJS
 
 ## Pages:
 
-- [ ] Home
-- [ ] Join
-- [ ] Login
-- [ ] Search
-- [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
-- [ ] Video Detail
-- [ ] Edit Video
-- [ ] 
+- Home
+- Join
+- Login
+- Search
+- User Detail
+- Edit Profile
+- Change Password
+- Upload
+- Video Detail
+- Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import routes from '../routes';
 import {
-  users,
   userDetail,
   editProfile,
   changePassword
@@ -9,9 +8,8 @@ import {
 
 export const userRouter = express.Router();
 
-userRouter.get(routes.users, users)
-userRouter.get(routes.userDetail, userDetail)
 userRouter.get(routes.editProfile, editProfile)
 userRouter.get(routes.changePassword, changePassword)
+userRouter.get(routes.userDetail, userDetail)
 
 export default userRouter;

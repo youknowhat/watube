d3aeb912a07ed229953ad5a8f183c72d0f10d252
Junghyun Park <junghyun.de@gmail.com>
2020-04-22 02:44:02 +0900

    Setup webpack configration

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { localMiddleware } from './middlewares';
-import userRouter from './routers/userRouter';
+import { userRouter } from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
+import { globalRouter } from './routers/globalRouter';
 import routes from './routes';
 
 const app = express();
 
 app.use(helmet());
 app.set('view engine', 'pug');
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static('static'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

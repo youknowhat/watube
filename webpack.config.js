2b16f811ed98af9597df0155f80bae033976b5c9
Junghyun Park <junghyun.de@gmail.com>
2020-04-23 23:58:57 +0900

    Styling header, footer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
-const autoPrefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer');
 const ExtractCSS = require('extract-text-webpack-plugin');
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js');
 const OUTPUT_DIR = path.join(__dirname, 'static');
+
 const config = {
   entry: ['@babel/polyfill', ENTRY_FILE],
   mode: MODE,
@@ -28,7 +29,7 @@ const config = {
             loader: 'postcss-loader',
             options: {
               plugins() {
-                return [autoPrefixer({ overrideBrowserslist: 'cover 99.5%' })];
+                return [autoprefixer({ browsers: 'cover 99.5%' })];
               },
             },
           },
@@ -43,10 +44,7 @@ const config = {
     path: OUTPUT_DIR,
     filename: '[name].js',
   },
-  plugins:
-  [
-    new ExtractCSS('styles.css'),
-  ],
+  plugins: [new ExtractCSS('styles.css')],
 };
 
 module.exports = config;

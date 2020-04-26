e05e2d66752d22e27ac55f773732f87e0e6e1d42
Junghyun Park <junghyun.de@gmail.com>
2020-04-26 23:09:09 +0900

    Remove Log

--- db.js
+++ db.js
@@ -10,8 +10,8 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 const db = mongoose.connection;
-const handleOpen = () => console.log('Connected to DB!');
-const handleError = (error) => console.log(`Error on DB Connection: ${error}`);
+// const handleOpen = () => console.log('Connected to DB!');
+// const handleError = (error) => console.log(`Error on DB Connection: ${error}`);
 
-db.once('open', handleOpen);
-db.on('error', handleError);
+// db.once('open', handleOpen);
+// db.on('error', handleError);

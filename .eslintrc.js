08b74e84dc12d2b165a74b21db9389d979e96dac
Junghyun Park <junghyun.de@gmail.com>
2020-04-29 22:43:09 +0900

    Set eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,18 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "no-console": 0,
+    "no-plusplus": 0,
+    "vars-on-top": 0,
+    "no-underscore-dangle": 0,
+    "comma-dangle": 0,
+    "func-names": 0,
+    "prefer-arrow-callback": 0,
+    "prefer-template": 0,
+    "no-nested-ternary": 0,
+    "max-classes-per-file": 0,
+    "arrow-parens": ["error", "as-needed"],
+    "no-restricted-syntax": [0, "ForOfStatement"],
+    "no-param-reassign": ["error", { "props": false }]
   },
 };
